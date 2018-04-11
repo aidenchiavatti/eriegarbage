bc4175820aaf141f8402155d7cc2a25ee917011d
Aiden-at-508687582768 <aidenchiavatti@gmail.com>
2018-04-11 14:07:03 -0400

    First commit

---
+++ src/main/java/com/eriegarbage/garbageapp/GarbageappApplication.java
@@ -0,0 +1,12 @@
+package com.eriegarbage.garbageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbageappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarbageappApplication.class, args);
+	}
+}

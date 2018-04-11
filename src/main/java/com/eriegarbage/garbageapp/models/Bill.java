03b31fa04038586885cd248c03b08da0e6588ec3
kchuz23 <kaytlynn.chuzie@gmail.com>
2018-04-11 16:00:27 -0400

    bill object

--- src/main/java/com/eriegarbage/garbageapp/models/Bill.java
+++ src/main/java/com/eriegarbage/garbageapp/models/Bill.java
@@ -1,4 +1,9 @@
 package com.eriegarbage.garbageapp.models;
 
 public class Bill {
+    private int billID;
+    private Payment payment;
+    private String dueDate;
+    private double total;
+    private ArrayList<Dispute> disputes;
 }

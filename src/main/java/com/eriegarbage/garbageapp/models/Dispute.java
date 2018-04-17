ae6df3619a065b897539b959e370bdace6f953bf
kchuz23 <kaytlynn.chuzie@gmail.com>
2018-04-17 14:40:49 -0400

    added details to dispute and payment class

--- src/main/java/com/eriegarbage/garbageapp/models/Dispute.java
+++ src/main/java/com/eriegarbage/garbageapp/models/Dispute.java
@@ -1,4 +1,14 @@
 package com.eriegarbage.garbageapp.models;
 
 public class Dispute {
+    private String dispute;
+    private String response;
+
+    public String viewDispute(){
+        return dispute;
+    }
+
+    public void addResponse(String r){
+        response = r;
+    }
 }
--- src/main/java/com/eriegarbage/garbageapp/models/Payment.java
+++ src/main/java/com/eriegarbage/garbageapp/models/Payment.java
@@ -1,4 +1,11 @@
 package com.eriegarbage.garbageapp.models;
 
 public class Payment {
+    private String date;
+    private double paymentTotal;
+
+    public String generateReceipt(){
+        //generate receipt and send to billing manager
+
+    }
 }

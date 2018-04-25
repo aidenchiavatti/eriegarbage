219b965ecd13a379b87176cd8bc2912259b21f77
karlenemccleary <karlenemccleary@gmail.com>
2018-04-25 17:37:39 -0400

    added checktype static analysis fixes

--- src/main/java/com/eriegarbage/garbageapp/dao/AccountDao.java
+++ src/main/java/com/eriegarbage/garbageapp/dao/AccountDao.java
@@ -1,14 +1,11 @@
 package com.eriegarbage.garbageapp.dao;
 
-import com.eriegarbage.garbageapp.dto.OverdueAccountDto;
 import com.eriegarbage.garbageapp.models.Account;
+import java.util.ArrayList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public interface AccountDao extends JpaRepository<Account, Long> {
     Account getAccountByUserName(String username);
@@ -18,13 +15,14 @@ public interface AccountDao extends JpaRepository<Account, Long> {
 
     @Modifying
     @Transactional
-    @Query("update Account a set a.firstName = ?1, a.lastName = ?2, a.address = ?3 where a.accountID = ?4")
-    void updateAccountInfo(String firstname, String lastName, String address, int accountID);
+    @Query("update Account a set a.firstName = ?1, a.lastName = ?2,"
+            + "a.address = ?3 where a.accountId = ?4")
+    void updateAccountInfo(String firstname, String lastName, String address, int accountId);
 
     @Modifying
     @Transactional
-    @Query("update Account a set a.requestSuspend = true where a.accountID = ?1")
-    void updateAccountSuspendRequest(int accountID);
+    @Query("update Account a set a.requestSuspend = true where a.accountId = ?1")
+    void updateAccountSuspendRequest(int accountId);
 
     @Modifying
     @Transactional
@@ -33,11 +31,11 @@ public interface AccountDao extends JpaRepository<Account, Long> {
 
     @Modifying
     @Transactional
-    @Query("update Account a set a.isSuspended = true where a.accountID = ?1")
-    void updateAccountSuspended(int accountID);
+    @Query("update Account a set a.isSuspended = true where a.accountId = ?1")
+    void updateAccountSuspended(int accountId);
 
     @Modifying
     @Transactional
-    @Query("delete from Account a where accountID = ?1")
-    void deleteAccount(int accountID);
+    @Query("delete from Account a where accountId = ?1")
+    void deleteAccount(int accountId);
 }
--- src/main/java/com/eriegarbage/garbageapp/dao/BillDao.java
+++ src/main/java/com/eriegarbage/garbageapp/dao/BillDao.java
@@ -1,26 +1,26 @@
 package com.eriegarbage.garbageapp.dao;
 
-import com.eriegarbage.garbageapp.models.Account;
 import com.eriegarbage.garbageapp.models.Bill;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
+
 
 /**
  * Created by karle on 4/17/2018.
  */
 public interface BillDao extends JpaRepository<Bill, Long> {
 
-   // @Query("insert into Billing (amount) value = (Bill)")
-//    @Query("select b from Bill b where b.dueDate > current_date")
-//    public Bill getBill();
+    // @Query("insert into Billing (amount) value = (Bill)")
+    // @Query("select b from Bill b where b.dueDate > current_date")
+    // public Bill getBill();
     //public void createBill(Bill billInfo);
-    Bill findByBillID(int billID);
+    Bill findByBillId(int billId);
 
     @Query("select b from Bill b join b.account a where a.userName =?1")
     List<Bill> findBillsByUsername(String username);
 
     @Query("select b from Bill b join b.account a where a.userName =?1 and b.total > 0")
-    List<Bill>findUnpaidBillsByUsername(String username);
+    List<Bill> findUnpaidBillsByUsername(String username);
 }
